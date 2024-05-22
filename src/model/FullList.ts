3394d3538a6658a39021fef6a19762ad1c95e12f
iiiamken <kenny.t.dinh93@gmail.com>
2024-05-23 00:25:54 +0200

    added load method

--- src/model/FullList.ts
+++ src/model/FullList.ts
@@ -4,13 +4,13 @@ interface List {
   list: ListItem[]
   load(): void
   save(): void
-  cleaList(): void
+  clearList(): void
   addItem(itemObj: ListItem): void
   removeItem(id: string): void
 }
 
 export default class FullList implements List {
-  static instance: FullList = new FullList() //new trick if we know we only going to have one list
+  static instance: FullList = new FullList() //trick if we know we only going to have one list
 
   constructor(private _list: ListItem[] = []) {}
 
@@ -23,6 +23,23 @@ export default class FullList implements List {
     return this._list
   }
 
+  load(): void {
+    const storedList: string | null = localStorage.getItem("list")
+    if (typeof storedList !== "string") return
+    //created similar type to ListItem but with underscores
+    const parsedList: { _id: string; _item: string; _checked: boolean }[] =
+      JSON.parse(storedList)
+
+    parsedList.forEach((itemObj) => {
+      const newListItem = new ListItem(
+        itemObj._id,
+        itemObj._item,
+        itemObj._checked
+      )
+      FullList.instance.addItem(newListItem)
+    })
+  }
+
   /**
    * Saves the list to the local storage by converting it to a JSON string and storing it under the key "list".
    *
@@ -32,8 +49,14 @@ export default class FullList implements List {
     localStorage.setItem("list", JSON.stringify(this._list))
   }
 
+  /**
+   * Removes an item from the list based on its ID and updates the local storage.
+   *
+   * @param {string} id - The ID of the item to be removed.
+   * @return {void} This function does not return anything.
+   */
   removeItem(id: string): void {
     this._list = this._list.filter((item) => item.id !== id)
-    this.save()
+    this.save() //updates our local storage
   }
 }
