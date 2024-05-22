32aa6a83855aeb8b9936bdddc388376679b06f9e
iiiamken <kenny.t.dinh93@gmail.com>
2024-05-22 23:32:05 +0200

    created a model for ListItem

--- src/model/ListItem.ts
+++ src/model/ListItem.ts
@@ -18,4 +18,20 @@ export default class ListItem implements Item {
   set id(id: string) {
     this._id = id
   }
+
+  get item(): string {
+    return this._item
+  }
+
+  set item(item: string) {
+    this._item = item
+  }
+}
+  get checked(): boolean {
+    return this._checked
+  }
+
+  set checked(checked: boolean) {
+    this._checked = checked
+  }
 }
