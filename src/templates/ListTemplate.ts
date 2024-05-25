e2da06f043a75224ab17e00464b18288d64ee0b2
iiiamken <kenny.t.dinh93@gmail.com>
2024-05-25 23:36:17 +0200

    added render method

--- src/templates/ListTemplate.ts
+++ src/templates/ListTemplate.ts
@@ -15,4 +15,38 @@ export default class ListTemplate implements DOMList {
   clear(): void {
     this.ul.innerHTML = ""
   }
+
+  render(fullList: FullList): void {
+    //clear the list
+    this.clear()
+
+    //for each item in the list
+    fullList.list.map((item) => {
+      //create a new list item
+      const li = document.createElement("li") as HTMLLIElement
+
+      //add a class to the list item   
+      li.className = "item"
+
+      //create a checkbox
+      const check = document.createElement("input") as HTMLInputElement
+      check.type = "checkbox"
+      check.id = item.id
+      check.tabIndex = 0
+      check.checked = item.checked
+      li.append(check)
+
+      //create a eventlistener for the checkbox
+      check.addEventListener("change", () => {
+        item.checked = !item.checked
+        fullList.save()
+
+        //create a label
+        const label = document.createElement("label") as HTMLLabelElement
+        label.htmlFor = item.id
+        label.textContent = item.item
+        li.append(label)
+        
+      })
+    })
 }
