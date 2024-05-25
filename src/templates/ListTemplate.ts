39336da90230508dcf5300ae1e9ce93445ff4867
iiiamken <kenny.t.dinh93@gmail.com>
2024-05-25 23:29:45 +0200

    added clear method

--- src/templates/ListTemplate.ts
+++ src/templates/ListTemplate.ts
@@ -1,14 +1,18 @@
 import FullList from "../model/FullList"
 
-interface DOMList{
-    ul: HTMLUListElement
-    clear(): void
-    render(fullList: FullList): void
+interface DOMList {
+  ul: HTMLUListElement
+  clear(): void
+  render(fullList: FullList): void
 }
 
-export default class ListTemplate implements DOMList{
+export default class ListTemplate implements DOMList {
+  ul: HTMLUListElement
+  private constructor() {
+    this.ul = document.getElementById("listItems") as HTMLUListElement
+  }
 
-
-    private constructor(){
-        this.ul = document.getElementById("listItems") as HTMLUListElement
-    }
+  clear(): void {
+    this.ul.innerHTML = ""
+  }
+}
