f9e2fe69c87801b722a4afbdd28d344fb0598c10
iiiamken <kenny.t.dinh93@gmail.com>
2024-05-26 23:12:52 +0200

    added code to main.ts

--- src/model/main.ts
+++ src/model/main.ts
@@ -1,1 +1,28 @@
 import "../css/style.css"
+
+import FullList from "./FullList"
+import ListItem from "./ListItem"
+import ListTemplate from "../templates/ListTemplate"
+
+const initApp = (): void => {
+  const fullList = FullList.instance
+  const template = ListTemplate.instance
+
+  const itemEntryForm = document.getElementById(
+    "itemEntryForm"
+  ) as HTMLFormElement
+  itemEntryForm.addEventListener("submit", (event: SubmitEvent): void => {
+    event.preventDefault
+  })
+
+  const clearItems = document.getElementById(
+    "clearItemsButton"
+  ) as HTMLButtonElement
+
+  clearItems.addEventListener("click", (): void => {
+    fullList.clearList()
+    template.clear()
+  })
+}
+
+document.addEventListener("DOMContentLoaded", initApp)
