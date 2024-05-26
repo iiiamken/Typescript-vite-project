c5a13834c6a8a9be7719c18975ecbca4933a2d8a
iiiamken <kenny.t.dinh93@gmail.com>
2024-05-26 23:20:31 +0200

    added eventlistener to form and clear button

--- src/model/main.ts
+++ src/model/main.ts
@@ -11,18 +11,39 @@ const initApp = (): void => {
   const itemEntryForm = document.getElementById(
     "itemEntryForm"
   ) as HTMLFormElement
+  //added event listener to itemEntryForm
   itemEntryForm.addEventListener("submit", (event: SubmitEvent): void => {
     event.preventDefault
+
+    const input = document.getElementById("newItem") as HTMLInputElement
+    const newEntryText: string = input.value.trim()
+
+    if (!newEntryText.length) return
+
+    const itemId: number = fullList.list.length
+      ? parseInt(fullList.list[fullList.list.length + 1].id + 1)
+      : 1
+
+    const newItem = new ListItem(itemId.toString(), newEntryText)
+    //add item to list
+    fullList.addItem(newItem)
+    //re-render the list
+    template.render(fullList)
   })
 
+  //clear button
   const clearItems = document.getElementById(
     "clearItemsButton"
   ) as HTMLButtonElement
-
+  //event listener for clear button
   clearItems.addEventListener("click", (): void => {
     fullList.clearList()
     template.clear()
   })
+  //init load the list
+  fullList.load()
+  //init render the list
+  template.render(fullList)
 }
 
 document.addEventListener("DOMContentLoaded", initApp)
